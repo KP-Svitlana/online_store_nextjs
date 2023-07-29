55e5322fb076d358801ebce1f1060bc176ee20b2
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-07-29 22:41:53 +0300

    Update page.js

--- src/app/signUp/page.js
+++ src/app/signUp/page.js
@@ -1,10 +1,11 @@
-import { LoginByGoogle } from "@/components/LoginByGoogle";
 import Link from "next/link";
+import { LoginByGoogle } from "@/components/LoginByGoogle";
 
 const Signup = () => {
   return (
     <>
       <h3 className="font-bold">SignUp Page</h3>
+
       <LoginByGoogle />
       <Link className="mr-2.5" href="/login">
         Login
