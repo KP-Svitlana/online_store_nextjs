6b2039d5339ae90e46fbf08e5c458759eb28bf18
alena <108529764+AlenaMushko@users.noreply.github.com>
2023-07-29 18:21:04 +0300

    add: Product, btn

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import ProductsList from "@/components/ProductsList/ProductsList";
 
 export default function Home() {
-  return (
-    <>
-      <h1 className="font-bold">Home Page</h1>
-      <Link className="mr-2.5"  href="/signUp">
-        Sign Up
-      </Link>
-      <Link href="/login">Login</Link>
-    </>
-  );
+    return (
+        <>
+            <>
+                <h1 className="font-bold">Home Page</h1>
+                <Link className="mr-2.5" href="/signUp">
+                    Sign Up
+                </Link>
+                <Link href="/login">Login</Link>
+            </>
+            <ProductsList/>
+        </>
+    );
 }
